e8d74d50ddb3a7885dcd53e2d2a4d3569cd37017
Lenin Yang <leninyang@gmail.com>
2017-05-25 11:30:38 -0700

    sign up working | updated user_controller POST route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,8 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    # @user = User.new(user_params)
+    @user = User.new(username:params[:user][:username], password:params[:user][:password])
 
     if @user.save
       render json: @user, status: :created, location: @user
